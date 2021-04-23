e70f84a376e82c581cc4208fa0d90861d10df856
LenorEric <40317566+LenorEric@users.noreply.github.com>
2021-04-24 02:33:39 +0800

    Update SubTrans.py

--- SubTrans.py
+++ SubTrans.py
@@ -1,17 +1,145 @@
+def template(AST):
+    LaTeXST = AST
+    return LaTeXST
+
+
+def transPlainText(AST):
+    LaTeXST = []
+    lines = AST[2]
+    for line in lines:
+        LaTeXST.append(['Text', line])
+    return LaTeXST
+
+
 def transImage(AST):
-    LaTeX_SP = []
+    LaTeXST = []
     begin = ['Env', ["figure", '[ht]']]
-    LaTeX_SP.append(begin)
+    LaTeXST.append(begin)
     if AST[1]["Centering"]:
-        LaTeX_SP.append(["Cmd", "\centering"])
-    include = "\includegraphics[width=" + str(AST[2][1]) + "\\textwidth]{" + AST[2][0] + "}"
-    LaTeX_SP.append(["Cmd", include])
+        LaTeXST.append(["Cmd", "\centering"])
+    include = "\includegraphics[width=" + str(AST[2][1]) + "\textwidth]{" + AST[2][0] + "}"
+    LaTeXST.append(["Cmd", include])
     if AST[2][2] != '':
         caption = '\caption{' + AST[2][2] + '}'
-        LaTeX_SP.append(['Cmd', caption])
-    LaTeX_SP.append(['Env', 'figure'])
-    return LaTeX_SP
+        LaTeXST.append(['Cmd', caption])
+    LaTeXST.append(['Env', ['\end{figure}']])
+    return LaTeXST
 
 
-if __name__ == '__main__':
-    print("This can only be imported")
+def transTabular(AST):
+    _table = {
+        'hline': ['Cmd', '\hline']
+    }
+    LaTeXST = [['Env', ["table", '[ht]']], ["Cmd", "\centering"]]
+    align = AST[1]["Align"]
+    coll = []
+    for i in range(len(align)):
+        coll.append('|')
+        coll.append(align[i])
+    coll.append('|')
+    begin = ['Env', ['tabular', '{' + ' '.join(coll) + '}']]
+    LaTeXST.append(begin)
+    LaTeXST.append(_table['hline'])
+    AST[2][0] = ["\\textbf{" + str(cell) + "}" for cell in AST[2][0]]
+    for line in AST[2]:
+        cells = ' & '.join([str(cell) for cell in line]) + " \\"
+        LaTeXST.append(['Text', cells])
+        LaTeXST.append(_table['hline'])
+    LaTeXST.append(['Env', ['\end{tabular}']])
+    LaTeXST.append(['Env', ['\end{figure}']])
+    return LaTeXST
+
+
+def transEnumerate(AST):
+    LaTeXST = [['Env', ["enumerate", '']]]
+    for line in AST[2]:
+        LaTeXST.append(['Cmd', '\item ' + str(line)])
+    LaTeXST.append(['Env', ['\end{enumerate}']])
+    return LaTeXST
+
+
+def transQuotation(AST):
+    LaTeXST = [['Env', ["quotation", '']]]
+    for line in AST[2]:
+        LaTeXST.append(['Text', str(line)])
+    LaTeXST.append(['Env', ['\end{quotation}']])
+    return LaTeXST
+
+
+def transItemize(AST):
+    LaTeXST = [['Env', ["itemize", '']]]
+    for line in AST[2]:
+        LaTeXST.append(['Cmd', '\item ' + str(line)])
+    LaTeXST.append(['Env', ['\end{itemize}']])
+    return LaTeXST
+
+
+def transFormula(AST):
+    def isZh(c):
+        if u'\u4e00' <= c <= u'\u9fff':
+            return True
+        return False
+
+    def findZh(str):
+        s = e = 0
+        for i in range(len(str)):
+            if isZh(str[i]):
+                e = i
+                if s == 0:
+                    s = i
+        return [s, e]
+
+    LaTeXST = []
+    for line in AST[2]:
+        text = line
+        index = findZh(line)
+        if index[0] != index[1]:
+            text = ''
+            for i in range(index[0]):
+                text += line[i]
+            text += ("\mbox{" + line[index[0]:index[1] + 1] + "}")
+            for i in range(index[1] + 1, len(line)):
+                text += line[i]
+        LaTeXST.append(['Text', text])
+    return LaTeXST
+
+# tabular=[
+#     "Tabular",
+#     {"Align":"lrrl"},
+#     [
+#         [1,2,3,4],
+#         [5,6,7,8]
+#     ]
+# ]
+
+# quotation=[ 
+#     "Quotation",
+#     {},
+#     [
+#         "@@@","$$$"
+#     ]
+# ]
+
+# Enumerate=[ 
+#     'Enumerate',
+#     {},
+#     ["12",'23','34']
+# ]
+
+# dollarDollar=[ 
+#     '$$',
+#     {},
+#     [
+#         "$$",
+#         '\\frac{aaa}{我是中文}',
+#         'a_{我是中文啊}',
+#         '$$'
+#     ]
+# ]
+# print(transTabular(tabular))
+# print(transQuotation(quotation))
+# print(transEnumerate(Enumerate))
+# print(transItemize(Enumerate))
+# print(findZh("12@中文12$2"))
+# print('123456'[0:2])
+# print(transFormula(dollarDollar))
